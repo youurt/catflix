c92d3ecef3f3871c17eee730006cb07919925380
Ugur Tigu <ugurtigu@gmail.com>
2020-12-25 16:30:12 +0100

    pulling data from firebase with custom hooks

--- src/pages/browse.js
+++ src/pages/browse.js
@@ -1,6 +1,14 @@
 import React from 'react';
+import { useContent } from '../hooks';
 
 const Browse = () => {
+  // series and films
+  const { films } = useContent('films');
+  const { series } = useContent('series');
+  console.log(series);
+  console.log(films);
+  // slides
+  // pass it to the browse container
   return <p>Hello from the Browse!</p>;
 };
 
